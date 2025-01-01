110f284271da4002c3cafb340fd150ec75d13cf7
TrinhChung <chungtrinh2k2@gmail.com>
2025-01-01 10:10:50 +0900

    test model

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,9 +1,67 @@
 "use client";
 
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Stats, ScrollControls } from "@react-three/drei";
+import React, { useRef, useLayoutEffect, useState } from "react";
+import { Canvas, applyProps, useFrame } from "@react-three/fiber";
+import {
+  AccumulativeShadows,
+  Stats,
+  PerformanceMonitor,
+  useGLTF,
+  RandomizedLight,
+  Environment,
+  Lightformer,
+  Float
+} from "@react-three/drei";
 import * as THREE from "three";
+import { LayerMaterial, Color, Depth } from "lamina";
+
+function Dress(props) {
+  const { scene, nodes, materials } = useGLTF("/models/911-transformed.glb");
+
+  console.log(nodes);
+  console.log(materials);
+
+   useLayoutEffect(() => {
+     Object.values(nodes).forEach(
+       (node) => node.isMesh && (node.receiveShadow = node.castShadow = true)
+     );
+     applyProps(materials.rubber, {
+       color: "#222",
+       roughness: 0.6,
+       roughnessMap: null,
+       normalScale: [4, 4],
+     });
+     applyProps(materials.window, {
+       color: "black",
+       roughness: 0,
+       clearcoat: 0.1,
+     });
+     applyProps(materials.coat, {
+       envMapIntensity: 4,
+       roughness: 0.5,
+       metalness: 1,
+     });
+     applyProps(materials.paint, {
+       envMapIntensity: 2,
+       roughness: 0.45,
+       metalness: 0.8,
+       color: "#555",
+     });
+   }, [nodes, materials]);
+
+   return <primitive object={scene} {...props} />;
+}
+
+function CameraRig({ v = new THREE.Vector3() }) {
+  return useFrame((state) => {
+    const t = state.clock.elapsedTime;
+    state.camera.position.lerp(
+      v.set(Math.sin(t / 5), 0, 12 + Math.cos(t / 5) / 2),
+      0.05
+    );
+    state.camera.lookAt(0, 0, 0);
+  });
+}
 
 function Cube() {
   const cubeRef = useRef();
@@ -22,7 +80,88 @@ function Cube() {
   );
 }
 
+function Lightformers({ positions = [2, 0, 2, 0, 2, 0, 2, 0] }) {
+  const group = useRef();
+  useFrame(
+    (state, delta) =>
+      (group.current.position.z += delta * 10) > 20 &&
+      (group.current.position.z = -60)
+  );
+  return (
+    <>
+      {/* Ceiling */}
+      <Lightformer
+        intensity={0.75}
+        rotation-x={Math.PI / 2}
+        position={[0, 5, -9]}
+        scale={[10, 10, 1]}
+      />
+      <group rotation={[0, 0.5, 0]}>
+        <group ref={group}>
+          {positions.map((x, i) => (
+            <Lightformer
+              key={i}
+              form="circle"
+              intensity={2}
+              rotation={[Math.PI / 2, 0, 0]}
+              position={[x, 4, i * 4]}
+              scale={[3, 1, 1]}
+            />
+          ))}
+        </group>
+      </group>
+      {/* Sides */}
+      <Lightformer
+        intensity={4}
+        rotation-y={Math.PI / 2}
+        position={[-5, 1, -1]}
+        scale={[20, 0.1, 1]}
+      />
+      <Lightformer
+        rotation-y={Math.PI / 2}
+        position={[-5, -1, -1]}
+        scale={[20, 0.5, 1]}
+      />
+      <Lightformer
+        rotation-y={-Math.PI / 2}
+        position={[10, 1, 0]}
+        scale={[20, 1, 1]}
+      />
+      {/* Accent (red) */}
+      <Float speed={5} floatIntensity={2} rotationIntensity={2}>
+        <Lightformer
+          form="ring"
+          color="red"
+          intensity={1}
+          scale={10}
+          position={[-15, 4, -18]}
+          target={[0, 0, 0]}
+        />
+      </Float>
+      {/* Background */}
+      <mesh scale={100}>
+        <sphereGeometry args={[1, 64, 64]} />
+        <LayerMaterial side={THREE.BackSide}>
+          <Color color="#444" alpha={1} mode="normal" />
+          <Depth
+            colorA="blue"
+            colorB="black"
+            alpha={0.5}
+            mode="normal"
+            near={0}
+            far={300}
+            origin={[100, 100, 100]}
+          />
+        </LayerMaterial>
+      </mesh>
+    </>
+  );
+}
+
+
 export default function Blog() {
+  const [degraded, degrade] = useState(false);
+
   return (
     <div
       style={{
@@ -34,22 +173,57 @@ export default function Blog() {
     >
       <Canvas
         style={{ width: "100%", height: "100%" }}
-        camera={{ position: [3, 3, 5], fov: 75 }}
+        shadows
+        camera={{ position: [5, 0, 15], fov: 30 }}
       >
         {/* Ánh sáng */}
-        <ambientLight intensity={1} />
+        <ambientLight intensity={0.5} />
         {/* Ánh sáng bổ sung */}
-        <directionalLight position={[10, 10, 10]} intensity={1} />
-
-        <fog attach="fog" args={["black", 15, 22.5]} />
+        <spotLight
+          position={[0, 15, 0]}
+          angle={0.3}
+          penumbra={1}
+          castShadow
+          intensity={2}
+          shadow-bias={-0.0001}
+        />
 
         {/* Hình khối */}
-        <ScrollControls damping={0.2} maxSpeed={0.5} pages={2}>
-          <Cube />
-        </ScrollControls>
+        {/* <Cube /> */}
+
+        <Dress
+          position={[-0.5, -0.18, 0]}
+          rotation={[0, Math.PI / 5, 0]}
+          scale={1.6}
+        />
+
+        <AccumulativeShadows
+          position={[0, -1.16, 0]}
+          frames={100}
+          alphaTest={0.9}
+          scale={10}
+        >
+          <RandomizedLight
+            amount={8}
+            radius={10}
+            ambient={0.5}
+            position={[1, 5, -1]}
+          />
+        </AccumulativeShadows>
+
+        <Environment
+          frames={degraded ? 1 : Infinity}
+          resolution={256}
+          background
+          blur={1}
+        >
+          <Lightformers />
+        </Environment>
+
+        <PerformanceMonitor onDecline={() => degrade(true)} />
 
         {/* Điều khiển */}
-        <OrbitControls enableZoom={true} />
+        <CameraRig />
 
         {/* Thống kê hiệu năng */}
         <Stats />
